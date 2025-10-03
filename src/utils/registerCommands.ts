6c9adc6198a411a739865df20eb62730225ff851
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2025-10-03 13:29:40 +0200

    fix: update command registration logic to support both global and guild-specific commands

--- src/utils/registerCommands.ts
+++ src/utils/registerCommands.ts
@@ -6,15 +6,23 @@ const commandsData = Object.values(commands).map((command) => command.data.toJSO
 
 export async function registerCommands() {
   try {
-    console.log(`Started refreshing ${commandsData.length} application (/) commands.`);
+    const guildId = config.discord.guildId;
+    const scope = guildId ? `guild ${guildId}` : "global";
+    console.log(`Started refreshing ${commandsData.length} ${scope} application commands.`);
 
     const rest = new REST({ version: "10" }).setToken(config.discord.token);
 
-    await rest.put(Routes.applicationCommands(config.discord.clientId), {
-      body: commandsData,
-    });
+    if (guildId) {
+      await rest.put(Routes.applicationGuildCommands(config.discord.clientId, guildId), {
+        body: commandsData,
+      });
+    } else {
+      await rest.put(Routes.applicationCommands(config.discord.clientId), {
+        body: commandsData,
+      });
+    }
 
-    console.log(`Successfully reloaded ${commandsData.length} globally (/) commands.`);
+    console.log(`Successfully reloaded ${commandsData.length} ${scope} commands.`);
   } catch (error) {
     console.error(error);
   }
