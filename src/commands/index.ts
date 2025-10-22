c33621a8e880bda35fc478d343a28ee6b8fcc6f6
Ali Hammoud <letsmailali.h@gmail.com>
2025-10-22 20:50:39 +0300

    🔨 refactor: update command imports and change commands structure to use Map

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,7 @@
-import { ping } from "./ping.js";
-import { reportMessage } from "./reportMessage.js";
+import { ping } from "./ping/index.js";
+import { reportMessage } from "./report-message/index.js";
+import type { Command } from "./types.js";
 
-export const commands = {
-  ping,
-  reportMessage: reportMessage,
-} as const;
+export const commands = new Map<string, Command>(
+  [ping, reportMessage].flat().map((command) => [command.data.name, command])
+);
