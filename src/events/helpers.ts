2490816877a9b67c3c0d99c3befd9c57ecc54c9a
Ali Hammoud <letsmailali.h@gmail.com>
2025-10-22 14:19:41 +0300

    🌟 feat: add helper for creating an event

---
+++ src/events/helpers.ts
@@ -0,0 +1,12 @@
+import type { ClientEvents } from "discord.js";
+import type { DiscordEvent } from "./types";
+
+export const createEvent = <T extends keyof ClientEvents = keyof ClientEvents>(
+  data: {
+    name: T;
+    once?: boolean;
+  },
+  execute: (...args: ClientEvents[T]) => Promise<void> | void
+): DiscordEvent<T> => {
+  return { ...data, execute };
+};
