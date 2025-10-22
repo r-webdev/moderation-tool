14ec48eb440bfe9bae2292538ff28f09ceba14cc
Ali Hammoud <letsmailali.h@gmail.com>
2025-10-22 20:58:08 +0300

    🔨 refactor: simplify index.ts by consolidating command imports and removing unused event handlers

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,12 @@
-import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import { config } from "./env.js";
-import { loadCommands } from "./utils/loadCommands.js";
-import { registerCommands } from "./utils/registerCommands.js";
+import { loadCommands, registerCommands } from "./utils/commands.js";
+import { loadEvents } from "./utils/events.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection();
 
 loadCommands(client);
+loadEvents(client);
 registerCommands();
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  /**
-   * Slash Commands
-   */
-  if (interaction.isChatInputCommand()) {
-    const command = client.commands.get(interaction.commandName);
-    if (!command) {
-      console.log(`Command not found ${interaction.commandName}`);
-      return;
-    }
-
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({
-        content: `There was an error while executing the ${interaction.commandName} command.`,
-        ephemeral: true,
-      });
-      console.log(`Error executing command ${interaction.commandName}`);
-      console.error(error);
-    }
-    return;
-  }
-
-  /**
-   * Message Context Menu Commands
-   */
-  if (interaction.isMessageContextMenuCommand()) {
-    const command = client.commands.get(interaction.commandName);
-    if (!command) {
-      console.log(`Command not found ${interaction.commandName}`);
-      return;
-    }
-
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      try {
-        await interaction.reply({
-          content: `There was an error while executing the ${interaction.commandName} command.`,
-          ephemeral: true,
-        });
-      } catch {
-        console.log(`Error replying to interaction ${interaction.commandName}`);
-      }
-    }
-  }
-});
-
-/**
- * Client Ready
- */
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
 
 client.login(config.discord.token);
