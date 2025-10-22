464d24216faf053f18b256925e13b6d044afa8da
Ali Hammoud <letsmailali.h@gmail.com>
2025-10-22 20:42:59 +0300

    🔨 refactor: add commandType to command types for better type safety

--- src/commands/types.ts
+++ src/commands/types.ts
@@ -1,4 +1,5 @@
 import type {
+  ApplicationCommandType,
   ChatInputCommandInteraction,
   MessageContextMenuCommandInteraction,
   RESTPostAPIChatInputApplicationCommandsJSONBody,
@@ -7,19 +8,26 @@ import type {
 } from "discord.js";
 
 export type UserContextMenuCommand = {
+  commandType: ApplicationCommandType.User;
   data: RESTPostAPIContextMenuApplicationCommandsJSONBody;
   execute: (interaction: UserContextMenuCommandInteraction) => Promise<void> | void;
 };
 
 export type MessageContextMenuCommand = {
+  commandType: ApplicationCommandType.Message;
   data: RESTPostAPIContextMenuApplicationCommandsJSONBody;
   execute: (interaction: MessageContextMenuCommandInteraction) => Promise<void> | void;
 };
 
 export type SlashCommand = {
+  commandType: ApplicationCommandType.ChatInput;
   data: RESTPostAPIChatInputApplicationCommandsJSONBody;
   execute: (interaction: ChatInputCommandInteraction) => Promise<void> | void;
 };
 
-export type ContextMenuCommand = UserContextMenuCommand | MessageContextMenuCommand;
-export type Command = SlashCommand | ContextMenuCommand;
+export type Command = SlashCommand | UserContextMenuCommand | MessageContextMenuCommand;
+
+export type CommandInteraction =
+  | ChatInputCommandInteraction
+  | MessageContextMenuCommandInteraction
+  | UserContextMenuCommandInteraction;
