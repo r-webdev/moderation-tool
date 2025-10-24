d7cf574a9467c455b88f9f9f1648415ab18c1436
Ali Hammoud <letsmailali.h@gmail.com>
2025-10-25 02:56:48 +0300

    🌟 feat: expand client intents for moderation

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ import { config } from "./env.js";
 import { loadCommands, registerCommands } from "./utils/commands.js";
 import { loadEvents } from "./utils/events.js";
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents:
+    GatewayIntentBits.Guilds |
+    GatewayIntentBits.GuildMessages |
+    GatewayIntentBits.MessageContent |
+    GatewayIntentBits.GuildModeration |
+    GatewayIntentBits.GuildMembers,
+});
 
 loadCommands(client);
 loadEvents(client);
