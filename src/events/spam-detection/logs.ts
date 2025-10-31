2ac85050172848b511f4d0616facfb1e825e88b6
Ali Hammoud <letsmailali.h@gmail.com>
2025-10-31 21:14:08 +0200

    🌟 feat: add logging functionality for spam detection events

---
+++ src/events/spam-detection/logs.ts
@@ -0,0 +1,72 @@
+import {
+  type Channel,
+  ContainerBuilder,
+  type Message,
+  MessageFlags,
+  SectionBuilder,
+  TextDisplayBuilder,
+  ThumbnailBuilder,
+  type User,
+} from "discord.js";
+
+const messagesMd = {
+  rule: (reason: string) => `**Rule Broken:** ${reason}`,
+  user: (author: User) => `**User:** <@${author.id}>`,
+  messagesInvolved: (messages: Message[], maxDisplay = 3) => {
+    const displayedMessages = messages.slice(0, maxDisplay);
+    const displayedCount = displayedMessages.length;
+    const remainingCount = messages.length - displayedMessages.length;
+    return (
+      `**Message${displayedCount > 1 ? "s" : ""} Involved:**\n` +
+      displayedMessages.map((msg) => `- "${msg.content}"`).join("\n") +
+      (remainingCount > 0 ? `  ...and ${remainingCount} more` : "")
+    );
+  },
+  channelsInvolved: (channels: Set<string>) => {
+    const channelMentions = Array.from(channels).map((id) => `<#${id}>`);
+    return `**Channel${channels.size > 1 ? "s" : ""} Involved:** ${channelMentions.join(", ")}`;
+  },
+  deletedAndMuted: (deletedCount: number, muteDuration?: number) => {
+    return `**Messages Deleted:** ${deletedCount}  **Mute Duration:** ${
+      muteDuration ? `${muteDuration / 1000} seconds` : "N/A"
+    }`;
+  },
+};
+export type LogFunction = (options: {
+  messages: Message[];
+  reason: string;
+  deletedMessagesCount: number;
+  muteDuration?: number;
+  logChannel?: Channel;
+}) => Promise<void>;
+
+export const defaultLogFunction: LogFunction = async (options) => {
+  if (!options.logChannel?.isSendable()) {
+    return;
+  }
+
+  const content = `${messagesMd.rule(options.reason)}
+${messagesMd.user(options.messages[0].author)}
+${messagesMd.messagesInvolved(options.messages)}
+${messagesMd.channelsInvolved(new Set(options.messages.map((msg) => msg.channelId)))}
+${messagesMd.deletedAndMuted(options.deletedMessagesCount, options.muteDuration)}`;
+
+  const textTextDisplayComponent = new TextDisplayBuilder().setContent(content);
+  console.log(content);
+
+  const sectionComponent = new SectionBuilder()
+    .addTextDisplayComponents(textTextDisplayComponent)
+    .setThumbnailAccessory(
+      new ThumbnailBuilder().setURL(options.messages[0].author.displayAvatarURL())
+    );
+
+  const containerComponent = new ContainerBuilder()
+    .addSectionComponents(sectionComponent)
+    .setAccentColor(0xff0000);
+
+  await options.logChannel.send({
+    flags: MessageFlags.IsComponentsV2,
+    allowedMentions: { users: undefined },
+    components: [containerComponent],
+  });
+};
