413c19ea59cd851c4ca40fc5f3394082968e4dcd
Ali Hammoud <letsmailali.h@gmail.com>
2025-10-31 23:28:31 +0200

    🌟 feat: add spam detection event to the events list

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,5 +1,6 @@
 import { interactionCreateEvent } from "./interaction-create/index.js";
 import { readyEvent } from "./ready/index.js";
+import { spamDetection } from "./spam-detection/index.js";
 import type { DiscordEvent } from "./types.js";
 
-export const events: DiscordEvent[] = [readyEvent, interactionCreateEvent];
+export const events: DiscordEvent[] = [readyEvent, interactionCreateEvent, spamDetection];
