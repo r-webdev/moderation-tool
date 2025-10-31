8b1da67e880e1418e93cd96cf2182b00de5ccd20
Ali Hammoud <letsmailali.h@gmail.com>
2025-10-31 23:28:48 +0200

    🌟 feat: add messagesCacheCommand to the commands list

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,8 @@
+import { messagesCacheCommand } from "./messages-cache/index.js";
 import { ping } from "./ping/index.js";
 import { reportMessage } from "./report-message/index.js";
 import type { Command } from "./types.js";
 
 export const commands = new Map<string, Command>(
-  [ping, reportMessage].flat().map((command) => [command.data.name, command])
+  [ping, reportMessage, messagesCacheCommand].flat().map((command) => [command.data.name, command])
 );
