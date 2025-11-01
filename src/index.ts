f9fd99b3e9247768e3dd263b11b2a1ec8adc6408
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2025-11-01 19:48:02 +0100

    feat: implement database connection and graceful shutdown in bot

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import { Client, GatewayIntentBits } from "discord.js";
+import { connectDatabase, disconnectDatabase } from "./database/operations.js";
 import { config } from "./env.js";
 import { loadCommands, registerCommands } from "./utils/commands.js";
 import { loadEvents } from "./utils/events.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// Connect to database before starting bot
+await connectDatabase();
+
 loadCommands(client);
 loadEvents(client);
 registerCommands();
+
+// Graceful shutdown
+process.on("SIGINT", async () => {
+  console.log("Shutting down...");
+  await disconnectDatabase();
+  process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+  console.log("Shutting down...");
+  await disconnectDatabase();
+  process.exit(0);
+});
 
 client.login(config.discord.token);
