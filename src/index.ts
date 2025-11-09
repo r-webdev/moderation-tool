12cacc9a5e1ebd57e15080038766299c57dc3efa
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2025-11-09 16:07:43 +0100

    chore: remove unused script.ts file and related database connection logic

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { Client, GatewayIntentBits } from "discord.js";
-import { connectDatabase, disconnectDatabase } from "./database/operations.js";
 import { config } from "./env.js";
 import { loadCommands, registerCommands } from "./utils/commands.js";
 import { loadEvents } from "./utils/events.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// Connect to database before starting bot
-await connectDatabase();
 
 loadCommands(client);
 loadEvents(client);
@@ -16,13 +12,11 @@ registerCommands();
 // Graceful shutdown
 process.on("SIGINT", async () => {
   console.log("Shutting down...");
-  await disconnectDatabase();
   process.exit(0);
 });
 
 process.on("SIGTERM", async () => {
   console.log("Shutting down...");
-  await disconnectDatabase();
   process.exit(0);
 });
 
